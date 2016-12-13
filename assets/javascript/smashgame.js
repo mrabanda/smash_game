1903fe6868fcbdca1d3a04f0f85047139d31f3b8
mrabanda <mrabanda@gmail.com>
2016-12-13 00:04:04 -0500

    added attack and counter attacks

--- assets/javascript/smashgame.js
+++ assets/javascript/smashgame.js
@@ -1,15 +1,62 @@
-function chooseCharacter() {
-	var mkdef = $('.character').not(this).removeClass('character').addClass('enemy');
-	$('.character').removeClass('character').data("player", "one");
-	$('.choose_opponent').append(mkdef);
-	$('.attacker').append($(this));
+var attackerHP;
+var defenderHP;
+var attack;
+var attackTotal;
+var counterAttack;
 
+
+function chooseCharacter() {
+	$('.character').off('click',chooseCharacter);
+	var mkdef = $('.character').not(this).removeClass('character').addClass('enemy');
+	console.log(mkdef);
+
+	$(this).removeClass('character');
+	$('.choose_opponent').append(mkdef);
+	$('.attacker').prepend($(this));
+	attackerHP = parseInt(this.dataset.hp);
+	attack = parseInt(this.dataset.attack);
+	attackTotal = attack;
+	console.log(attackerHP);
+
+	console.log("DeezNutz");
 };
 
 function chooseOpponent() {
-	// $('.defender').append($(this));
-	// $(this).appendTo('.defender');
+	$('.defender').append($(this));
+	defenderHP = parseInt(this.dataset.hp);
+	counterAttack = parseInt(this.dataset.attack);
+	$('button').show();
+
+	console.log(defenderHP);
+	console.log('Ballz')
 };
 
-$('.character').click(chooseCharacter);
-$('.enemy').click(chooseOpponent);
+function attackOpponent() {
+	if(attackerHP >= 0 && defenderHP >= 0) {
+		defenderHP = defenderHP - attackTotal;
+		if(defenderHP > 0) {
+			attackerHP = attackerHP - counterAttack;
+			if(attackerHP <= 0) {
+				$('.attack_text').html("You Suck, Refresh To Play Again");
+			}
+			else {
+				$('.attack_text').html("You attacked the enemey with " + attackTotal + " damage and sustained " + counterAttack + " damage")
+				attackTotal = attackTotal + attack;
+			}
+		} else {
+			$('.defender').empty();
+		}
+		console.log("Sack");
+		console.log("attackHP =",attackerHP);
+  	console.log("defenderHP =",defenderHP);
+  	console.log(attackTotal)
+	}
+} 
+
+  
+
+$('.character').one('click',chooseCharacter);
+$('.choose_opponent').on('click', '.enemy',chooseOpponent);
+
+$('.btn').on('click',attackOpponent);
+  
